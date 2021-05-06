8ffe7445c030c1b819d66327d0e13b2d28622d14
Dmitry Blinov <dev.blinov@gmail.com>
2021-05-06 09:52:02 +0300

    Initial Commit

---
+++ Hubly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hubly
+//
+//  Created by Dmitry Blinov on 06.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hubly/HublyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HublyApp.swift
+//  Hubly
+//
+//  Created by Dmitry Blinov on 06.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct HublyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
